0ccadc29eda9803136b5e81b38facce768334838
Julian Matthews <perseus1718@gmail.com>
2022-10-11 19:52:32 -0400

    Add workspace and sleep classes to testing1.py

--- backend/testing1.py
+++ backend/testing1.py
@@ -8,8 +8,9 @@
 
 
 from psonic import *
-import random
-from os import path
+import random 
+import os
+
 
 set_server_parameter('127.0.0.1',4557,4559)
 
@@ -18,23 +19,25 @@ def switch_slashes(s):
 
 class ForLoop:
     def __init__(self, sleeptime=1, iterations=1, sleepstep=0):
-        self.samples = []
+        self.blocks = []
         self.sleeptime = sleeptime
         self.iterations = iterations
         self.sleepstep = sleepstep
 
-    def add_sample(self, sample):
-        self.samples.append(sample)
-    
+    def add_block(self, sample):
+        self.blocks.append(sample)
+
     def add_sleeptime(self, sleeptime):
         self.sleeptime += sleeptime
+        if sleeptime < 0:
+            self.sleeptime = 0
     
     def play(self):
         for i in range(self.iterations):
-            for s in self.samples:
-                s.play()
-                self.add_sleeptime(self.sleepstep)
-                sleep(self.sleeptime)
+            for b in self.blocks:
+                b.play()
+            self.add_sleeptime(self.sleepstep)
+            sleep(self.sleeptime)
 
 class Sample:
     def __init__(self, path, rate=1, ratestep=0, amp=1, attack=0, release=0, start=0, finish=1):
@@ -46,7 +49,7 @@ class Sample:
         self.release = release
         self.start = start
         self.finish = finish
-    
+
     def add_rate(self, rate):
         self.rate += rate
 
@@ -54,9 +57,39 @@ class Sample:
         self.add_rate(self.ratestep)
         sample(self.path, attack=self.attack, release=self.release, rate=self.rate, amp=self.amp, start=self.start, finish=self.finish)
 
+class Sleep:
+    def __init__(self, sleeptime=1, sleepstep=0):
+        self.sleeptime = sleeptime
+        self.sleepstep = sleepstep
+
+    def add_sleeptime(self, sleeptime):
+        was = self.sleeptime
+        self.sleeptime += sleeptime
+        if self.sleeptime < 0:
+            self.sleeptime = was
+
+    def play(self):
+        self.add_sleeptime(self.sleepstep)
+        sleep(self.sleeptime)
+
+class Workspace:
+    def __init__(self):
+        self.loops = []
+
+    def add_loop(self, loop):
+        self.loops.append(loop)
+
+    def play(self):
+        for b in self.loops:
+            b.play()
 
 
-key_slime = Sample(path.abspath('backend/samples/key_slime.wav'), rate=4, ratestep=-0.5)
-loop1 = ForLoop(sleeptime=0.2, iterations=6, sleepstep=0.1)
-loop1.add_sample(key_slime)
-loop1.play()
+loop1 = ForLoop(sleeptime=0, iterations=6, sleepstep=0)
+for path in os.listdir('backend/samples'):
+    path = os.path.abspath('backend/samples/' + path)
+    loop1.add_block(Sample(path, rate=4, ratestep=-0.5))
+    loop1.add_block(Sleep(sleeptime=0.2, sleepstep=-0.03))
+
+workspace = Workspace()
+workspace.add_loop(loop1)
+workspace.play()
