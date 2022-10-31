d12d3921ee5e66b7028b024974d50e0d39b8fd25
Julian <matthj8@rpi.edu>
2022-10-31 17:26:36 -0400

    add initial tests

--- backend/test/test1.py
+++ backend/test/test1.py
@@ -12,20 +12,52 @@ def read_yaml_file_to_dict(filename):
 def yaml_read():
     print(read_yaml_file_to_dict('backend/yaml_format_example.yaml'))
 
-def test1():
+def test_start_block():
     set_server_parameter('127.0.0.1', 4557, 4559)
-    loop1 = Loop(sleeptime=0.5, iterations=16)
-    sample1 = Sample(path=os.path.abspath('backend\samples\key_slime.wav'))
-    loop1.addSubBlock(sample1)
-    modifier = deltaRate(rateDelta=0.5)
-    modifier2 = deltaSleeptime(delta=-0.1)
-    modifier3 = deltaFinish(finishDelta=-0.03)
-    sample1.addModifier(modifier)
-    sample1.addModifier(modifier3)
-    loop1.addModifier(modifier2)
-
     start = Start()
+    sample1 = Sample(path=os.path.abspath('backend\samples\key_slime.wav'))
+    start.addSubBlock(sample1)
+    start.play()
+
+def test_start_block_2():
+    set_server_parameter('127.0.0.1', 4557, 4559)
+    start = Start()
+    sample1 = Sample(path=os.path.abspath('backend\samples\key_slime.wav'))
+    loop1 = Loop(sleeptime=0.5, iterations=4)
+    loop1.addSubBlock(sample1)
     start.addSubBlock(loop1)
     start.play()
 
-test1()
+def test_loop():
+    set_server_parameter('127.0.0.1', 4557, 4559)
+    loop1 = Loop(sleeptime=0.5, iterations=5)
+    sample1 = Sample(path=os.path.abspath('backend\samples\key_slime.wav'))
+    loop1.addSubBlock(sample1)
+    loop1.play()
+
+def test_deltaSleeptime():
+    set_server_parameter('127.0.0.1', 4557, 4559)
+    loop1 = Loop(sleeptime=0.5, iterations=5)
+    sample1 = Sample(path=os.path.abspath('backend\samples\key_slime.wav'))
+    loop1.addSubBlock(sample1)
+    modifier = deltaSleeptime(delta=-0.1)
+    loop1.addModifier(modifier)
+    loop1.play()
+
+def test_deltaRate():
+    set_server_parameter('127.0.0.1', 4557, 4559)
+    loop1 = Loop(sleeptime=0.5, iterations=4)
+    sample1 = Sample(path=os.path.abspath('backend\samples\key_slime.wav'))
+    loop1.addSubBlock(sample1)
+    modifier = deltaRate(rateDelta=0.5)
+    sample1.addModifier(modifier)
+    loop1.play()
+
+
+if __name__ == '__main__':
+    test_start_block_2()
+    test_loop()
+    test_deltaSleeptime()
+    test_deltaRate()
+
+
