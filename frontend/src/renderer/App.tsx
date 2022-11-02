108e82860b9e652ffcbdbb15e02d68362c7ceec5
amortensen1229 <mortensen1229@gmail.com>
2022-11-02 11:33:40 -0400

    fixed import bug

--- frontend/src/renderer/App.tsx
+++ frontend/src/renderer/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
 import './styles/App.css';
 import UploadFile from './Menu/UploadFile';
 import Canvas from './Canvas/Canvas';
