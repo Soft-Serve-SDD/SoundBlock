deee185e79daf27eccac73ab4789aed83726ade2
Jenny Gao <jennyg4416@gmail.com>
2022-11-02 11:22:36 -0400

    play wav files

---
+++ frontend/src/renderer/Blocks/draggableBlocks.jsx
@@ -0,0 +1,20 @@
+import { renderers } from './fields';
+import { Draggable } from '../Components/draggable';
+
+export function PlayBlock() {
+  const Component = renderers['play'];
+  return (
+    <Draggable>
+      <Component />
+    </Draggable>
+  );
+}
+
+export function SampleBlock() {
+  const Component = renderers['sample'];
+  return (
+    <Draggable>
+      <Component />
+    </Draggable>
+  );
+}
