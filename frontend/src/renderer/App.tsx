7debf98c632dde485f99899fa6f2b085154f28b0
Julian Matthews <perseus1718@gmail.com>
2022-11-02 11:37:30 -0400

    Add play button back

--- frontend/src/renderer/App.tsx
+++ frontend/src/renderer/App.tsx
@@ -1,11 +1,14 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/App.css';
 import UploadFile from './Menu/UploadFile';
+import PlayButton from './Menu/PlayButton';
 import Canvas from './Canvas/Canvas';
+
 const Hello = () => {
   return (
     <div>
       <UploadFile />
+      <PlayButton />
       <Canvas />
     </div>
   );
