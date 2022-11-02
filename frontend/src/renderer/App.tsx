399f26c6b1e5819f4b22d54b0be26a7ee1d9a329
Jenny Gao <jennyg4416@gmail.com>
2022-11-02 15:27:00 -0400

    revert prettier

--- frontend/src/renderer/App.tsx
+++ frontend/src/renderer/App.tsx
@@ -5,37 +5,28 @@ import UploadFile from './Menu/UploadFile';
 import PlayButton from './Menu/PlayButton';
 import Canvas from './Canvas/Canvas';
 
+
 const Container = () => {
   return (
     <React.Fragment>
-      <div style={{ width: '100%' }}>
-        <h1 style={{ textAlign: 'center', marginBottom: '0px' }}>
-          Sound Blocks
-        </h1>
-        <br />
-        <h2 style={{ textAlign: 'center', marginTop: '0px' }}>
-          Music Editing Software
-        </h2>
+      <div style={{width: "100%"}}>
+        <h1 style={{textAlign: "center", marginBottom: "0px"}}>Sound Blocks</h1> 
+        <br/> 
+        <h2 style={{textAlign: "center", marginTop: "0px"}}>Music Editing Software</h2>
       </div>
-      <div
-        style={{
-          display: 'flex',
-          paddingTop: '25px',
-          justifyContent: 'center',
-        }}
-      >
+      <div style={{display: "flex", paddingTop: "25px", justifyContent: "center"}}>
         <BlockMenu />
-        <WorkSpace />
-        <SoundLibrary />
+        <WorkSpace/>
+        <SoundLibrary/>
       </div>
     </React.Fragment>
-  );
-};
+  )
+}
 
 const WorkSpace = () => {
   return (
-    <div style={{ width: '50%', background: 'grey' }}>
-      <h1>WorkSpace</h1>
+    <div style={{width: "50%", background: "grey"}}>
+      <h1 >WorkSpace</h1>
       <UploadFile />
       <PlayButton />
       <Canvas />
@@ -45,25 +36,28 @@ const WorkSpace = () => {
 
 const BlockMenu = () => {
   return (
-    <div style={{ width: '25%', background: 'white' }}>
+    <div style={{width: "25%", background: "white"}}>
       <h1>Block Menu</h1>
     </div>
-  );
-};
+  )
+}
 
 const SoundLibrary = () => {
   return (
-    <div style={{ width: '25%', background: 'white' }}>
+    <div style={{width: "25%", background: "white"}}>
       <h1>Sound Library</h1>
     </div>
-  );
-};
+  )
+}
+
+
+
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Container />} />
+        <Route path="/" element={<Container/>} />
       </Routes>
     </Router>
   );
