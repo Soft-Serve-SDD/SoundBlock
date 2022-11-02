93135229bcdf8caef283112f743b5a850e94c560
Julian Matthews <perseus1718@gmail.com>
2022-11-02 13:27:40 -0400

    Remove the knob for now

--- frontend/src/renderer/components/Knob.jsx
+++ frontend/src/renderer/components/Knob.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Knob } from 'primereact/knob';
+// import React from 'react';
+// import { Knob } from 'primereact/knob';
 
-export default function KnobComponent() {
-  const [value, setValue] = React.useState(0);
+// export default function KnobComponent() {
+//   const [value, setValue] = React.useState(0);
 
-  const onChange = (e) => {
-    setValue(e.value);
-  };
+//   const onChange = (e) => {
+//     setValue(e.value);
+//   };
 
-  return (
-    <div>
-      <Knob
-        value={value}
-        onChange={onChange}
-        valueColor={'Turquoise'}
-        rangeColor={'White'}
-        rangeWidth={10}
-        // size={50}
-      />
-      {value}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <Knob
+//         value={value}
+//         onChange={onChange}
+//         valueColor={'Turquoise'}
+//         rangeColor={'White'}
+//         rangeWidth={10}
+//         // size={50}
+//       />
+//       {value}
+//     </div>
+//   );
+// }
