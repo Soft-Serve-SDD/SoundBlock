4843760ad547ae6f4ee08f4125069383c5bf3c73
Jenny Gao <jennyg4416@gmail.com>
2022-11-20 10:43:25 -0500

    Knobs branch (#23)
    * Trying out knobs instead of sliders. Add some comments to backend.
    
    * Attempting knob layout
    
    * import and testing of react-dial-knob
    
    * Good looking block with all properties. Knobs not adjusting quite right.
    
    * Delete node_modules directory
    
    Co-authored-by: Julian <matthj8@rpi.edu>
    Co-authored-by: Julian Matthews <perseus1718@gmail.com>

--- frontend/src/renderer/Blocks/draggableBlocks.jsx
+++ frontend/src/renderer/Blocks/draggableBlocks.jsx
@@ -2,6 +2,9 @@ import { renderers } from './fields';
 import { Draggable } from '../Components/draggable';
 import Slider from '../Components/Slider';
 import { useState } from 'react';
+// import simple knobs 
+import { Basic } from 'react-dial-knob'
+import React from 'react';
 export function PlayBlock() {
   const Component = renderers['play'];
   return (
@@ -20,29 +23,113 @@ export function SampleBlock() {
   );
 }
 
-export function AudioBlock(Props) {
+export function AudioBlock(Props) {   
   const { adjustProperties, blockInfo } = Props
   console.log(blockInfo)
   const [name, setName ] = useState(blockInfo.name)
   const [file, setFile] = useState(blockInfo.file)
 
-  const adjustRate = (rate) => {
+  
+  const setRate = (rate) => {
     adjustProperties(blockInfo, rate)
   }
 
+  const setAmp = (amp) => {
+    adjustProperties(blockInfo, amp)
+  }
+
+  const setAttack = (attack) => {
+    adjustProperties(blockInfo, attack)
+  }
+
+  const setRelease = (release) => {
+    adjustProperties(blockInfo, release)
+  }
+
+  const setStart = (start) => {
+    adjustProperties(blockInfo, start)
+  }
+
+  const setFinish = (finish) => {
+    adjustProperties(blockInfo, finish)
+  }
+
+  const diameter = 65
+
   return (
-    <div style={{ border: '1px solid black', borderRadius: '5px' }}>
-      <h4 style={{ marginTop: '5px', marginBottom: '0px' }}>{name}</h4>
-      <br />
-      <h5 style={{ marginTop: '5px', marginBottom: '0px' }}>
-        {' '}
-        Duration: {Props.duration}
-      </h5>
-      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-        <div>
-          Rate
-          <Slider adjustRate={adjustRate}/>
-        </div>
+    // 3 x 2 grid of Basic knobs
+    <div style={{display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridTemplateRows: 'repeat(2, 1fr)'}}>
+      <div style={{gridColumn: '1', gridRow: '1'}}>
+        <Basic
+          diameter={diameter}
+          min={0}
+          max={10}
+          step={1}
+          value={blockInfo.rate}
+          onValueChange={setRate}
+          ariaLabelledBy={'rate'}
+        />
+        <label id={'rate'}>Rate</label>
+      </div>
+      <div style={{gridColumn: '2', gridRow: '1'}}>
+        <Basic
+          diameter={diameter}
+          min={0}
+          max={10}
+          step={1}
+          value={blockInfo.amp}
+          onValueChange={setAmp}
+          ariaLabelledBy={'amp'}
+        />
+        <label id={'amp'}>Amp</label>
+      </div>
+      <div style={{gridColumn: '3', gridRow: '1'}}>
+        <Basic
+          diameter={diameter}
+          min={0}
+          max={10}
+          step={1}
+          value={blockInfo.attack}
+          onValueChange={setAttack}
+          ariaLabelledBy={'attack'}
+        />
+        <label id={'attack'}>Attack</label>
+      </div>
+      <div style={{gridColumn: '1', gridRow: '2'}}>
+        <Basic
+          diameter={diameter}
+          min={0}
+          max={10}
+          step={1}
+          value={blockInfo.release}
+          onValueChange={setRelease}
+          ariaLabelledBy={'release'}
+        />
+        <label id={'release'}>Release</label>
+      </div>
+      <div style={{gridColumn: '2', gridRow: '2'}}>
+        <Basic
+          diameter={diameter}
+          min={0}
+          max={10}
+          step={1}
+          value={blockInfo.start}
+          onValueChange={setStart}
+          ariaLabelledBy={'start'}
+        />
+        <label id={'start'}>Start</label>
+      </div>
+      <div style={{gridColumn: '3', gridRow: '2'}}>
+        <Basic
+          diameter={diameter}
+          min={0}
+          max={10}
+          step={1}
+          value={blockInfo.finish}
+          onValueChange={setFinish}
+          ariaLabelledBy={'finish'}
+        />
+        <label id={'finish'}>Finish</label>
       </div>
     </div>
   );
--- frontend/src/renderer/Components/Slider.jsx
+++ frontend/src/renderer/Components/Slider.jsx
@@ -25,3 +25,7 @@ const Slider = (props) => {
 };
 
 export default Slider;
+
+
+
+
