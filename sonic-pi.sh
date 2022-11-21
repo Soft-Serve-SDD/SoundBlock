26d3c64bb60b22b6c4424c368fd9ad2b965e462a
Joe Chu <chujoe26@gmail.com>
2022-11-21 17:20:13 -0500

    SB-52: Linux and windows executables tested

---
+++ sonic-pi.sh
@@ -0,0 +1,9 @@
+echo "Preparing to run Sonic-Pi Frontend"
+cd frontend
+npm install
+echo "Running Sonic-Pi Frontend"
+npm start &
+cd ../backend
+echo "Running Sonic-Pi Backend"
+python3 main.py &
+
